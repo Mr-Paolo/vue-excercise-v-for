d242c2285dea6f48c16a45f14968609a58383888
Paolo Cargnin <paolocargnin@boom.ink>
2021-04-23 10:14:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+data() {
+    return: {
+        cars: [{
+            price: 80000,
+            brand: 'Mercedes',
+            name: 'Class E'
+        },{
+            price: 40000,
+            brand: 'Jeep',
+            name: 'Wrangler'
+        },{
+            price: 10000,
+            brand: 'Fiat',
+            name: 'Panda'
+        },{
+            price: 60000,
+            brand: 'Tesla',
+            name: 'Model 3'
+        },{
+            price: 30000,
+            brand: 'Ford',
+            name: 'Mustang'
+        }]
+    
+}
